08b4c2592ab2454ab4d179f963bdf6c3084eef94
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-06 20:25:15 +0300

    UPD User classer adding basic methods

--- ClinicSystemBusiness/User.cs
+++ ClinicSystemBusiness/User.cs
@@ -1,12 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class User
+    public class User
     {
+        private enum ModeUser { Add, Update };
+        private ModeUser _Mode;
+        public Person Person { get; private set; }//composition
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Permission { get; set; }
+
+        public User()
+        {
+            Person Person = new Person();
+            this.Id = -1;
+            this.PersonId = -1;
+            this.UserName = string.Empty;
+            this.Password = string.Empty;
+            this.Permission = 0;
+            _Mode = ModeUser.Add;
+        }
+        private User(int Id, int PersonId, string UerName, string Password, int Permission)
+        {
+            Person = Person.Find(PersonId);
+            this.Id = Id;
+            this.PersonId = PersonId;
+            this.UserName = UerName;
+            this.Password = Password;
+            this.Permission = Permission;
+            _Mode = ModeUser.Update;
+        }
+
+        private bool _Add()
+        {
+            //validation
+            this.Id = UserData.Add(this.UserName, this.Password, this.Permission, this.PersonId);
+            return (Id != -1);
+        }
+        private bool _Update()
+        {
+            //validation
+            return UserData.Update(this.Id, this.UserName, this.Password, this.Permission, this.PersonId);
+        }
+        private bool _ReadyUser()
+        {
+            if (!ExistByUserName(this.UserName) && _Mode == ModeUser.Update)
+            {
+                return false;
+            }
+            if (!PersonData.Exist(this.PersonId) || string.IsNullOrWhiteSpace(this.Password) || this.Permission == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool Save()
+        {
+            if (!_ReadyUser())
+            {
+                return false;
+            }
+            switch (_Mode)
+            {
+                case ModeUser.Add:
+                    return _Add();
+                case ModeUser.Update:
+                    return _Update();
+            }
+            return false;
+        }
+        public static bool Delete(int Id)
+        {
+            return UserData.Delete(Id);
+        }
+        public static DataTable All()
+        {
+            return UserData.All();
+        }
+        public static User Find(string UserName)
+        {
+            int Id = -1;
+            int PersonId = -1;
+            string Password = string.Empty;
+            int Permission = 0;
+
+            if (UserData.GetUserByUserName(ref PersonId, ref Id, UserName, ref Password, ref Permission))
+            {
+                return new User(Id, PersonId, UserName, Password, Permission);
+            }
+            return null;
+        }
+        public static User FindById(int UserId)
+        {
+            string UserName = string.Empty;
+            int PersonId = -1;
+            string Password = string.Empty;
+            int Permission = 0;
+
+            if (UserData.GetUserById(ref PersonId, UserId, ref UserName, ref Password, ref Permission))
+            {
+                return new User(UserId, PersonId, UserName, Password, Permission);
+            }
+            return null;
+        }
+        static public User FindByUserNameAndPassword(string UserName, string Password)
+        {
+            int Id = -1;
+            int PersonId = -1;
+            int Permission = 0;
+            if (UserData.GetUserByUserNameAndPassword(ref PersonId, ref Id, UserName, Password, ref Permission))
+            {
+                return new User(Id, PersonId, UserName, Password, Permission);
+            }
+            return null;
+        }
+        public static bool Exist(int Id)
+        {
+            return UserData.Exist(Id);
+        }
+        public static bool ExistByUserName(string UserName)
+        {
+            return UserData.ExistByUserName(UserName);
+        }
+
     }
 }
--- ClinicSystemDataAccess/UserData.cs
+++ ClinicSystemDataAccess/UserData.cs
@@ -1,12 +1,181 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClinicSystemDataAccess
 {
-    internal class UserData
+    public class UserData
     {
+        static public int Add(string username, string password, int Permission, int PersonId)
+        {
+            int NewIdCustomer = 0;
+            string query = @"insert into Users (UserName,password,Permission,PersonId) 
+                        values (@username,@password,@Permission,@PersonId)
+                           SELECT SCOPE_IDENTITY();";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@Permission", Permission);
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+                    try
+                    {
+                        connection.Open();
+                        object Result = command.ExecuteScalar();
+                        if (Result != null && int.TryParse(Result.ToString(), out int insertedId))
+                        {
+                            NewIdCustomer = insertedId;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return NewIdCustomer;
+        }
+        static public bool Update(int Id, string username, string password, int Permission, int PersonId)
+        {
+            int RowAffected = 0;
+            string query = @"   update Users set UserName=@username , password=@password,Permission=@Permission,PersonId=@PersonId
+				   where Id=@Id;";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserName", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@Permission", Permission);
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        RowAffected = command.ExecuteNonQuery();
+                    }
+                    catch { Exception exception; }
+                }
+            }
+            return RowAffected > 0;
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("Delete Users where Id=@Id", "@Id", Id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from View_Users_Details");
+        }
+        static public bool GetUserByUserNameAndPassword(ref int PersonId, ref int Id, string username, string Password, ref int Permission)
+        {
+            string query = @"select * from Users where UserName=@username and Password=@Password";
+
+            bool IsFound = false;
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserName", username);
+                    command.Parameters.AddWithValue("@Password", Password);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            PersonId = (int)reader["Person Id"];
+                            Permission = (int)reader["Permission"];
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+
+                        reader.Close();
+                    }
+                    catch { Exception exception; }
+                }
+            }
+            return IsFound;
+        }
+        static public bool GetUserByUserName(ref int PersonId, ref int Id, string username, ref string Password, ref int Permission)
+        {
+            string query = @"select * from Users where UserName=@username";
+
+            bool IsFound = false;
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserName", username);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            PersonId = (int)reader["Person Id"];
+                            Password = (string)reader["Password"];
+                            Permission = (int)reader["Permission"];
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                        reader.Close();
+                    }
+                    catch { Exception exception; }
+                }
+            }
+            return IsFound;
+        }
+        static public bool GetUserById(ref int PersonId, int Id, ref string username, ref string Password, ref int Permission)
+        {
+            string query = @"select * from Users where Id=@Id";
+            bool IsFound = false;
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            username = (string)reader["UserName"];
+                            PersonId = (int)reader["Person Id"];
+                            Password = (string)reader["Password"];
+                            Permission = (int)reader["Permission"];
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                        reader.Close();
+                    }
+                    catch { Exception exception; }
+                }
+            }
+            return IsFound;
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from Users where Id =@Id", "@Id", Id);
+        }
+        static public bool ExistByUserName(string UserName)
+        {
+            return GenericData.Exist("select Found=1 from Users where UserName=@UserName", "@UserName", UserName);
+        }
     }
 }
