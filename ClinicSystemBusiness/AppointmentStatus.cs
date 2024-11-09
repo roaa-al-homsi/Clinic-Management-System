e66e5e3668160a8a6b4655ef5c2b15c552c419b2
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 09:15:30 +0300

    UPD AppointmentStatus adding basic methods

---
+++ ClinicSystemBusiness/AppointmentStatus.cs
@@ -0,0 +1,69 @@
+﻿using ClinicSystemDataAccess;
+using System.Data;
+
+namespace ClinicSystemBusiness
+{
+    internal class AppointmentStatus
+    {
+        private enum Mode { Add, Update };
+        private Mode _mode;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public AppointmentStatus()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+            _mode = Mode.Add;
+        }
+        public AppointmentStatus(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = AppointmentStatusData.Add(this.Name);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return AppointmentStatusData.Update(this.Id, this.Name);
+        }
+        public bool Save()
+        {
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static AppointmentStatus Find(int id)
+        {
+            string name = string.Empty;
+            if (AppointmentStatusData.GetAppointmentStatus(id, ref name))
+            {
+                return new AppointmentStatus(id, name);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return AppointmentStatusData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return AppointmentStatusData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            return AppointmentStatusData.Delete(id);
+        }
+    }
+}
---
+++ ClinicSystemDataAccess/AppointmentStatusData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClinicSystemDataAccess
+{
+    public static class AppointmentStatusData
+    {
+        public static int Add(string name)
+        {
+            int newId = -1;
+            string query = @"insert into AppointmentStatus (Name)values(@Name)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("name", name);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+
+                }
+            }
+            return newId;
+        }
+        public static bool Update(int id, string name)
+        {
+            int rowsAffected = 0;
+            string query = @"update AppointmentStatus set Name=@name where Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete AppointmentStatus where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from AppointmentStatus where id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from AppointmentStatus");
+        }
+        static public bool GetAppointmentStatus(int id, ref string name)
+        {
+            bool isFound = false;
+            string query = @"select * from AppointmentStatus";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            name = (string)reader["Name"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
+    }
+}
