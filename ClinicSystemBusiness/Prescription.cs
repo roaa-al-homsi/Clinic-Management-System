5f781fcfa7c9bb3e8aec055253a5513c24af98b9
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 20:06:13 +0300

    UPD Prescriptions classes adding basic methods

--- ClinicSystemBusiness/Prescription.cs
+++ ClinicSystemBusiness/Prescription.cs
@@ -1,12 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class Prescription
+    public class Prescription
     {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public string MedicationName { get; set; }
+        public string Dosage { get; set; }
+        public string Frequency { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string SpecialInstructions { get; set; }
+        public int MedicalRecordId { get; set; }
+        public MedicalRecord MedicalRecord { get; private set; }
+
+        public Prescription()
+        {
+            this.MedicalRecordId = -1;
+            this.Id = -1;
+            this.Dosage = string.Empty;
+            this.Frequency = string.Empty;
+            this.StartDate = DateTime.MinValue;
+            this.EndDate = DateTime.MinValue;
+            this.SpecialInstructions = string.Empty;
+            this.MedicationName = string.Empty;
+            MedicalRecord = new MedicalRecord();
+            _mode = Mode.Add;
+        }
+        private Prescription(int id, string dosage, string frequency, DateTime startDate, DateTime endTime, string specialInstructions, string medicationName, int medicalRecordId)
+        {
+            this.Id = id;
+            this.Dosage = dosage;
+            this.Frequency = frequency;
+            this.StartDate = startDate;
+            this.EndDate = endTime;
+            this.SpecialInstructions = specialInstructions;
+            this.MedicationName = medicationName;
+            this.MedicalRecordId = medicalRecordId;
+            this.MedicalRecord = MedicalRecord.Find(MedicalRecordId);
+            _mode = Mode.Update;
+        }
+
+        private bool _Add()
+        {
+            this.Id = PrescriptionData.Add(this.Dosage, this.Frequency, this.StartDate, this.EndDate, this.SpecialInstructions, this.MedicationName, this.MedicalRecordId);
+            return (Id != -1);
+        }
+        private bool _Update()
+        {
+            return PrescriptionData.Update(this.Id, this.Dosage, this.Frequency, this.StartDate, this.EndDate, this.SpecialInstructions, this.MedicationName, this.MedicalRecordId);
+        }
+        private bool _ReadyPrescription()
+        {
+            if ((!Exist(this.Id) && _mode == Mode.Update) ||
+               string.IsNullOrWhiteSpace(this.Frequency) ||
+               string.IsNullOrWhiteSpace(this.Dosage) ||
+               string.IsNullOrWhiteSpace(this.MedicationName) ||
+               string.IsNullOrWhiteSpace(this.SpecialInstructions) ||
+               this.StartDate == DateTime.MinValue || this.EndDate == DateTime.MinValue ||
+                !MedicalRecord.Exist(this.MedicalRecordId))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool Save()
+        {
+            if (!_ReadyPrescription())
+            {
+                return false;
+            }
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Delete(int Id)
+        {
+            return PrescriptionData.Delete(Id);
+        }
+        public static DataTable All()
+        {
+            return PrescriptionData.All();
+        }
+        public static Prescription Find(string PrescriptionName)
+        {
+            int id = -1;
+            string dosage = string.Empty;
+            string frequency = string.Empty;
+            DateTime startDate = DateTime.MinValue;
+            DateTime endTime = DateTime.MinValue;
+            string specialInstructions = string.Empty;
+            string medicationName = string.Empty;
+            int medicalRecordId = -1;
+
+            if (PrescriptionData.GetPrescription(id, ref dosage, ref frequency, ref startDate, ref endTime, ref specialInstructions, ref medicationName, ref medicalRecordId))
+            {
+                return new Prescription(id, dosage, frequency, startDate, endTime, specialInstructions, medicationName, medicalRecordId);
+            }
+            return null;
+        }
+        public static bool Exist(int Id)
+        {
+            return PrescriptionData.Exist(Id);
+        }
+
     }
 }
--- ClinicSystemDataAccess/PrescriptionData.cs
+++ ClinicSystemDataAccess/PrescriptionData.cs
@@ -1,12 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClinicSystemDataAccess
 {
-    internal class PrescriptionData
+    public static class PrescriptionData
     {
+        public static int Add(string dosage, string frequency, DateTime startDate, DateTime endTime, string specialInstructions, string medicationName, int medicalRecordId)
+        {
+            int newPrescriptionsId = -1;
+            string query = @"insert into Prescriptions (MedicationName,medicalRecordId,Dosage,StartDate,EndDate,Frequency,SpecialInstructions)values
+                                                (@medicationName,@medicalRecordId,@dosage,@startDate,@endTime,@frequency,@specialInstructions)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@medicationName", medicationName);
+                    command.Parameters.AddWithValue("@medicalRecordId", medicalRecordId);
+                    command.Parameters.AddWithValue("@dosage", dosage);
+                    command.Parameters.AddWithValue("@startDate", startDate);
+                    command.Parameters.AddWithValue("@endTime", endTime);
+                    command.Parameters.AddWithValue("@frequency", frequency);
+                    command.Parameters.AddWithValue("@specialInstructions", specialInstructions);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newPrescriptionsId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return newPrescriptionsId;
+        }
+        public static bool Update(int id, string dosage, string frequency, DateTime startDate, DateTime endTime, string specialInstructions, string medicationName, int medicalRecordId)
+        {
+            int rowsAffected = 0;
+            string query = @"update Prescriptions set
+               dosage=@dosage,frequency=@frequency,startDate=@startDate,endTime=@endTime,
+               specialInstructions=@specialInstructions,medicationName=@medicationName,medicalRecordId=@medicalRecordId
+                                   where Id=@id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@dosage", dosage);
+                    command.Parameters.AddWithValue("@frequency", frequency);
+                    command.Parameters.AddWithValue("@startDate", startDate);
+                    command.Parameters.AddWithValue("@endTime", endTime);
+                    command.Parameters.AddWithValue("@specialInstructions", specialInstructions);
+                    command.Parameters.AddWithValue("@medicationName", medicationName);
+                    command.Parameters.AddWithValue("@medicalRecordId", medicalRecordId);
+
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Prescriptions where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Prescriptions where Id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Prescriptions");
+        }
+        static public bool GetPrescription(int id, ref string dosage, ref string frequency, ref DateTime startDate, ref DateTime endTime, ref string specialInstructions, ref string medicationName, ref int medicalRecordId)
+        {
+            bool isFound = false;
+            string query = @"select * from Prescriptions where Id=@id";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            dosage = (string)reader["Dosage"];
+                            frequency = (string)reader["Frequency"];
+                            startDate = (DateTime)reader["StartDate"];
+                            endTime = (DateTime)reader["EndTime"];
+                            specialInstructions = (string)reader["SpecialInstructions"];
+                            medicationName = (string)reader["MedicationName"];
+                            medicalRecordId = (int)reader["medicalRecordId"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
     }
 }
