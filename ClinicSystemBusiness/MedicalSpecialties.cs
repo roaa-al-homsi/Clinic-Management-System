cc438a7810de7b8f4407ebd88202d32ce07aed63
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 09:11:20 +0300

    UPD MedicalSpecialties adding basic methods

--- ClinicSystemBusiness/MedicalSpecialties.cs
+++ ClinicSystemBusiness/MedicalSpecialties.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class MedicalSpecialties
+    public class MedicalSpecialties
     {
+        private enum Mode { Add, Update };
+        private Mode _mode;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public MedicalSpecialties()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+            _mode = Mode.Add;
+        }
+        public MedicalSpecialties(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = MedicalSpecialtiesData.Add(this.Name);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return MedicalSpecialtiesData.Update(this.Id, this.Name);
+        }
+        public bool Save()
+        {
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static MedicalSpecialties Find(int id)
+        {
+            string name = string.Empty;
+            if (MedicalSpecialtiesData.GetMedicalSpecialties(id, ref name))
+            {
+                return new MedicalSpecialties(id, name);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return MedicalSpecialtiesData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return MedicalSpecialtiesData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            return MedicalSpecialtiesData.Delete(id);
+        }
+
+
     }
 }
--- ClinicSystemDataAccess/MedicalSpecialtiesData.cs
+++ ClinicSystemDataAccess/MedicalSpecialtiesData.cs
@@ -1,6 +1,98 @@
-﻿namespace ClinicSystemDataAccess
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClinicSystemDataAccess
 {
-    internal class MedicalSpecialtiesData
+    public static class MedicalSpecialtiesData
     {
+        public static int Add(string name)
+        {
+            int newId = -1;
+            string query = @"insert into MedicalSpecialties (Name)values(@Name)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("name", name);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+
+                }
+            }
+            return newId;
+        }
+        public static bool Update(int id, string name)
+        {
+            int rowsAffected = 0;
+            string query = @"update MedicalSpecialties set Name=@name where Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete MedicalSpecialties where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from MedicalSpecialties where id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from MedicalSpecialties");
+        }
+        static public bool GetMedicalSpecialties(int id, ref string name)
+        {
+            bool isFound = false;
+            string query = @"select * from MedicalSpecialties";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            name = (string)reader["Name"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
     }
 }
