fa0c432fecd7a37472bb25015c877459a2211e90
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 09:19:08 +0300

    UPD PaymentMethods classes adding basic methods

--- ClinicSystemBusiness/PaymentMethods.cs
+++ ClinicSystemBusiness/PaymentMethods.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class PaymentMethods
+    public class PaymentMethods
     {
+        private enum Mode { Add, Update };
+        private Mode _mode;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public PaymentMethods()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+            _mode = Mode.Add;
+        }
+        public PaymentMethods(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = PaymentMethodsData.Add(this.Name);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return PaymentMethodsData.Update(this.Id, this.Name);
+        }
+        public bool Save()
+        {
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static PaymentMethods Find(int id)
+        {
+            string name = string.Empty;
+            if (PaymentMethodsData.GetPaymentMethods(id, ref name))
+            {
+                return new PaymentMethods(id, name);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return PaymentMethodsData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return PaymentMethodsData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            return PaymentMethodsData.Delete(id);
+        }
     }
 }
