b0055d41efbd1dd287e6e0b7a6e2954b3ae33cdd
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 10:32:11 +0300

    UPD Patients classes adding basic methods

--- ClinicSystemBusiness/Patient.cs
+++ ClinicSystemBusiness/Patient.cs
@@ -1,12 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class Patient
+    public class Patient
     {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+
+        public Patient()
+        {
+            Person = new Person();
+            this.Id = -1;
+            this.PersonId = -1;
+            _mode = Mode.Add;
+        }
+        private Patient(int id, int personId)
+        {
+            Person = Person.Find(personId);
+            Id = id;
+            PersonId = personId;
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = PatientData.Add(this.PersonId);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return PatientData.Update(this.Id, this.PersonId);
+        }
+        public bool Save()
+        {
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static Patient Find(int id)
+        {
+            int personId = -1;
+            if (PatientData.GetPatient(id, ref personId))
+            {
+                return new Patient(id, personId);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return PatientData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return PatientData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            int personId = PatientData.GetPersonIdByPatientId(id);
+            if (!PersonData.Delete(personId))
+            {
+                return false;
+            }
+            else
+            {
+                return PatientData.Delete(id);
+            }
+        }
+
+
     }
 }
--- ClinicSystemDataAccess/PatientData.cs
+++ ClinicSystemDataAccess/PatientData.cs
@@ -1,6 +1,118 @@
-﻿namespace ClinicSystemDataAccess
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClinicSystemDataAccess
 {
-    internal class PatientData
+    public static class PatientData
     {
+        public static int Add(int personId)
+        {
+            int newPatientsId = -1;
+            string query = @"insert into Patients (PersonId)values(@personId)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("personId", personId);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newPatientsId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return newPatientsId;
+        }
+        public static bool Update(int id, int personId)
+        {
+            int rowsAffected = 0;
+            string query = @"update Patients set PersonId=@personId where Id=@id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@personId", personId);
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Patients where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Patients where id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Patients");
+        }
+        static public bool GetPatient(int id, ref int personId)
+        {
+            bool isFound = false;
+            string query = @"select * from Patients";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            personId = (int)reader["PersonId"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
+        static public int GetPersonIdByPatientId(int patientId)
+        {
+            int personID = 0;
+            string query = @"select PersonId from Patients where Id =@patientId";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@patientId", patientId);
+                    try
+                    {
+                        connection.Open();
+                        object Result = command.ExecuteScalar();
+                        if (Result != null && int.TryParse(Result.ToString(), out int insertedId))
+                        {
+                            personID = insertedId;
+                        }
+                    }
+                    catch { Exception exception; }
+                }
+            }
+            return personID;
+        }
     }
 }
