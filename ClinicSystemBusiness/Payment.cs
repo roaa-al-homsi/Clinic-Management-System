41154d89b5a1de69a0a7d9dcb74fc01732c96b55
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 17:49:41 +0300

    UPD Payments classes adding basic methods

--- ClinicSystemBusiness/Payment.cs
+++ ClinicSystemBusiness/Payment.cs
@@ -1,12 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class Payment
+    public class Payment
     {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+        public string AdditionalNotes { get; set; }
+        public int PaymentMethodsId { get; set; }
+        public PaymentMethods PaymentMethods { get; set; }
+
+        public Payment()
+        {
+            this.Id = -1;
+            this.PaymentMethodsId = -1;
+            this.Amount = 0;
+            this.AdditionalNotes = string.Empty;
+            this.PaymentMethodsId = 0;
+            this.Date = DateTime.MinValue;
+            this.PaymentMethods = new PaymentMethods();
+            _mode = Mode.Add;
+        }
+        private Payment(int id, DateTime date, int amount, string additionalNotes, int paymentMethodId)
+        {
+            PaymentMethods = PaymentMethods.Find(paymentMethodId);
+            this.PaymentMethodsId = paymentMethodId;
+            this.Id = id;
+            this.Date = date;
+            this.Amount = amount;
+            this.AdditionalNotes = additionalNotes;
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = PaymentsData.Add(this.Date, this.Amount, this.AdditionalNotes, this.PaymentMethodsId);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return PaymentsData.Update(this.Id, this.Date, this.Amount, this.AdditionalNotes, this.PaymentMethodsId);
+        }
+        private bool _ReadyPayment()
+        {
+            if (this.Date == DateTime.MinValue || this.Amount <= 0 || !PaymentMethodsData.Exist(this.PaymentMethodsId))
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool Save()
+        {
+            if (!_ReadyPayment())
+            {
+                return false;
+            }
+
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static Payment Find(int id)
+        {
+            int amount = 0;
+            DateTime date = DateTime.MinValue;
+            string additionalNotes = string.Empty;
+            int paymentMethodId = -1;
+            if (PaymentsData.GetPayment(id, ref date, ref amount, ref additionalNotes, ref paymentMethodId))
+            {
+                return new Payment(id, date, amount, additionalNotes, paymentMethodId);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return PaymentsData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return PaymentsData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else
+            {
+                return PaymentsData.Delete(id);
+            }
+        }
     }
 }
--- ClinicSystemDataAccess/PaymentsData.cs
+++ ClinicSystemDataAccess/PaymentsData.cs
@@ -1,12 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClinicSystemDataAccess
 {
-    internal class PaymentsData
+    public static class PaymentsData
     {
+        public static int Add(DateTime date, int amount, string additionalNotes, int PaymentMethodsId)
+        {
+            int newPaymentsId = -1;
+            string query = @"insert into Payments (date,amount,AdditionalNotes,PaymentMethodsId)values(@date,@amount,@additionalNotes,@PaymentMethodsId)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@date", date);
+                    command.Parameters.AddWithValue("@amount", amount);
+                    command.Parameters.AddWithValue("@PaymentMethodsId", PaymentMethodsId);
+                    command.Parameters.AddWithValue("@additionalNotes", (!string.IsNullOrEmpty(additionalNotes) ? additionalNotes : (object)System.DBNull.Value));
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newPaymentsId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return newPaymentsId;
+        }
+        public static bool Update(int id, DateTime date, int amount, string additionalNotes, int PaymentMethodsId)
+        {
+            int rowsAffected = 0;
+            string query = @"update Payments set amount=@amount,additionalNotes=@additionalNotes,PaymentMethodsId=@PaymentMethodsId where Id=@id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@amount", amount);
+                    command.Parameters.AddWithValue("@PaymentMethodsId", PaymentMethodsId);
+                    command.Parameters.AddWithValue("@AdditionalNotes", !string.IsNullOrWhiteSpace(additionalNotes) ? additionalNotes : (object)System.DBNull.Value);
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Payments where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Payments where Id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Payments");
+        }
+        static public bool GetPayment(int id, ref DateTime date, ref int amount, ref string additionalNotes, ref int PaymentMethodsId)
+        {
+            bool isFound = false;
+            string query = @"select * from Payments";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            date = (DateTime)reader["Date"];
+                            amount = (int)reader["Amount"];
+                            PaymentMethodsId = (int)reader["PaymentMethodsId"];
+                            additionalNotes = (reader["AdditionalNotes"] == System.DBNull.Value) ? string.Empty : (string)reader["AdditionalNotes"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
+
     }
 }
