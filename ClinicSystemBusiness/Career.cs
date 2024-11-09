52f4c3e9a76cde33ebdce8ed61cbaff2950a52f0
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 09:04:18 +0300

    UPD Carrer adding basic methods

--- ClinicSystemBusiness/Career.cs
+++ ClinicSystemBusiness/Career.cs
@@ -1,14 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
     public class Career
     {
+        private enum Mode { Add, Update };
+        private Mode _mode;
+        public int Id { get; set; }
+        public string Name { get; set; }
 
-        public Career() { } 
+        public Career()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+            _mode = Mode.Add;
+        }
+        public Career(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = CareerData.Add(this.Name);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return CareerData.Update(this.Id, this.Name);
+        }
+        public bool Save()
+        {
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static Career Find(int id)
+        {
+            string name = string.Empty;
+            if (CareerData.GetCareer(id, ref name))
+            {
+                return new Career(id, name);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return CareerData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return CareerData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            return CareerData.Delete(id);
+        }
     }
 }
+
--- ClinicSystemDataAccess/CarrerData.cs
+++ ClinicSystemDataAccess/CarrerData.cs
@@ -1,6 +1,99 @@
-﻿namespace ClinicSystemDataAccess
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClinicSystemDataAccess
 {
-    internal class CareerData
+    public static class CareerData
     {
+        public static int Add(string name)
+        {
+            int newCareerId = -1;
+            string query = @"insert into CareerSpecializations (Name)values(@Name)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("name", name);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newCareerId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+
+                }
+            }
+            return newCareerId;
+        }
+        public static bool Update(int id, string name)
+        {
+            int rowsAffected = 0;
+            string query = @"update CareerSpecializations set Name=@name where Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete CareerSpecializations where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from CareerSpecializations where id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from CareerSpecializations");
+        }
+        static public bool GetCareer(int id, ref string name)
+        {
+            bool isFound = false;
+            string query = @"select * from CareerSpecializations";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            name = (string)reader["Name"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
     }
 }
+
