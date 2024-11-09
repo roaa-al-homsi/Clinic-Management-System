8009d53357db15c68fc6e2b3f3bc6bce1c3eb73f
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-09 15:13:40 +0300

    UPD Doctors classes adding basic methods

--- ClinicSystemBusiness/Doctor.cs
+++ ClinicSystemBusiness/Doctor.cs
@@ -1,12 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicSystemDataAccess;
+using System.Data;
 
 namespace ClinicSystemBusiness
 {
-    internal class Doctor
+    public class Doctor
     {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int MedicalSpecialtiesId { get; set; }
+        public int EmployeeId { get; set; }
+        MedicalSpecialties MedicalSpecialties { get; set; }
+        Employee Employee { get; set; }
+
+        public Doctor()
+        {
+            this.Id = -1;
+            this.EmployeeId = -1;
+            this.MedicalSpecialtiesId = -1;
+            this.MedicalSpecialties = new MedicalSpecialties();
+            this.Employee = new Employee();
+            _mode = Mode.Add;
+        }
+        private Doctor(int id, int medicalSpecId, int employeeId)
+        {
+            this.Id = id;
+            this.MedicalSpecialtiesId = medicalSpecId;
+            this.EmployeeId = employeeId;
+
+            this.MedicalSpecialties = MedicalSpecialties.Find(medicalSpecId);
+            this.Employee = Employee.Find(employeeId);
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = DoctorData.Add(this.MedicalSpecialtiesId, this.EmployeeId);
+            return (this.Id == -1);
+        }
+        private bool _Update()
+        {
+            return DoctorData.Update(this.Id, this.MedicalSpecialtiesId, this.EmployeeId);
+        }
+        public bool Save()
+        {
+            switch (_mode)
+            {
+                case Mode.Add: return _Add();
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static Doctor Find(int id)
+        {
+            int employeeId = -1;
+            int specialtiesId = -1;
+
+            if (DoctorData.GetDoctor(id, ref specialtiesId, ref employeeId))
+            {
+                return new Doctor(id, specialtiesId, employeeId);
+            }
+            return null;
+        }
+        public static DataTable All()
+        {
+            return DoctorData.All();
+        }
+        public static bool Exist(int id)
+        {
+            return DoctorData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            int employeeId = DoctorData.GetEmployeeIdByDoctorId(id);
+            if (!DoctorData.Delete(id))
+            {
+                return false;
+            }
+            else
+            {
+                return EmployeeData.Delete(employeeId);
+            }
+        }
+
+
     }
 }
--- ClinicSystemDataAccess/DoctorData.cs
+++ ClinicSystemDataAccess/DoctorData.cs
@@ -1,12 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClinicSystemDataAccess
 {
-    internal class DoctorData
+    public static class DoctorData
     {
+        public static int Add(int SpecializationId, int employeeId)
+        {
+            int newDoctorsId = -1;
+            string query = @"insert into Doctors (SpecializationId,employeeId)values(@SpecializationId,@employeeId)
+                          SELECT SCOPE_IDENTITY();";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@SpecializationId", SpecializationId);
+                    command.Parameters.AddWithValue("@employeeId", employeeId);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newDoctorsId = insertedID;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return newDoctorsId;
+        }
+        public static bool Update(int id, int SpecializationId, int employeeId)
+        {
+            int rowsAffected = 0;
+            string query = @"update Doctors set SpecializationId=@SpecializationId,employeeId=@employeeId where Id=@id";
+
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@employeeId", employeeId);
+                    command.Parameters.AddWithValue("@SpecializationId", SpecializationId);
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return rowsAffected > 0;
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Doctors where Id =@id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Doctors where id =@id", "@id", id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Doctors");
+        }
+        static public bool GetDoctor(int id, ref int SpecializationId, ref int employeeId)
+        {
+            bool isFound = false;
+            string query = @"select * from Doctors";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            SpecializationId = (int)reader["SpecializationId"];
+                            employeeId = (int)reader["EmployeeId"];
+                        }
+                        else
+                        {
+                            isFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            return isFound;
+        }
+        static public int GetEmployeeIdByDoctorId(int doctorId)
+        {
+            int employeeId = 0;
+            string query = @"select EmployeeId from Doctors where Id =@doctorId";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@DoctorId", doctorId);
+                    try
+                    {
+                        connection.Open();
+                        object Result = command.ExecuteScalar();
+                        if (Result != null && int.TryParse(Result.ToString(), out int insertedId))
+                        {
+                            employeeId = insertedId;
+                        }
+                    }
+                    catch { Exception exception; }
+                }
+            }
+            return employeeId;
+        }
     }
 }
