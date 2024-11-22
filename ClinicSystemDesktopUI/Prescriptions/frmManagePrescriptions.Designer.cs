a9838c5c40d07152545dc343a6f72fe01b39b0f0
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-11-23 01:44:44 +0300

    ADD frmManagePrescriptions and view all prescriptions

---
+++ ClinicSystemDesktopUI/Prescriptions/frmManagePrescriptions.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ClinicSystem.Prescriptions
+{
+    partial class frmManagePrescriptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvManagePrescriptions = new Guna.UI2.WinForms.Guna2DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvManagePrescriptions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvManagePrescriptions
+            // 
+            this.dgvManagePrescriptions.AllowDrop = true;
+            this.dgvManagePrescriptions.AllowUserToAddRows = false;
+            this.dgvManagePrescriptions.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(48)))), ((int)(((byte)(52)))));
+            this.dgvManagePrescriptions.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvManagePrescriptions.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dgvManagePrescriptions.BackgroundColor = System.Drawing.Color.Silver;
+            this.dgvManagePrescriptions.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(16)))), ((int)(((byte)(18)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Andalus", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.NullValue = "NULL";
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(0, 1, 0, 0);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvManagePrescriptions.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvManagePrescriptions.ColumnHeadersHeight = 30;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Andalus", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(117)))), ((int)(((byte)(119)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvManagePrescriptions.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvManagePrescriptions.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(56)))), ((int)(((byte)(62)))));
+            this.dgvManagePrescriptions.Location = new System.Drawing.Point(5, 12);
+            this.dgvManagePrescriptions.Name = "dgvManagePrescriptions";
+            this.dgvManagePrescriptions.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvManagePrescriptions.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvManagePrescriptions.RowHeadersVisible = false;
+            this.dgvManagePrescriptions.RowHeadersWidth = 51;
+            this.dgvManagePrescriptions.RowTemplate.Height = 24;
+            this.dgvManagePrescriptions.Size = new System.Drawing.Size(733, 343);
+            this.dgvManagePrescriptions.TabIndex = 3;
+            this.dgvManagePrescriptions.Theme = Guna.UI2.WinForms.Enums.DataGridViewPresetThemes.Dark;
+            this.dgvManagePrescriptions.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(48)))), ((int)(((byte)(52)))));
+            this.dgvManagePrescriptions.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dgvManagePrescriptions.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dgvManagePrescriptions.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dgvManagePrescriptions.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dgvManagePrescriptions.ThemeStyle.BackColor = System.Drawing.Color.Silver;
+            this.dgvManagePrescriptions.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(56)))), ((int)(((byte)(62)))));
+            this.dgvManagePrescriptions.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(16)))), ((int)(((byte)(18)))));
+            this.dgvManagePrescriptions.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvManagePrescriptions.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Andalus", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvManagePrescriptions.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvManagePrescriptions.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvManagePrescriptions.ThemeStyle.HeaderStyle.Height = 30;
+            this.dgvManagePrescriptions.ThemeStyle.ReadOnly = true;
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Andalus", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.Height = 24;
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(117)))), ((int)(((byte)(119)))));
+            this.dgvManagePrescriptions.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.White;
+            // 
+            // frmManagePrescriptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 412);
+            this.Controls.Add(this.dgvManagePrescriptions);
+            this.Name = "frmManagePrescriptions";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Manage Prescriptions";
+            this.Load += new System.EventHandler(this.frmManagePrescriptions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvManagePrescriptions)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2DataGridView dgvManagePrescriptions;
+    }
+}
---
+++ ClinicSystemDesktopUI/Prescriptions/frmManagePrescriptions.cs
@@ -0,0 +1,22 @@
+﻿using ClinicSystemBusiness;
+using System;
+using System.Windows.Forms;
+
+namespace ClinicSystem.Prescriptions
+{
+    public partial class frmManagePrescriptions : Form
+    {
+        public frmManagePrescriptions()
+        {
+            InitializeComponent();
+        }
+        private void _RefreshPrescriptionsData()
+        {
+            dgvManagePrescriptions.DataSource = Prescription.All();
+        }
+        private void frmManagePrescriptions_Load(object sender, EventArgs e)
+        {
+            _RefreshPrescriptionsData();
+        }
+    }
+}
